2a05ab908eabc0c462d27ac3757971e1470d2e80
Berk Balatacı <32279212+berkbltc@users.noreply.github.com>
2019-04-18 22:52:27 +0200

    Update WriteWordToFile.java

--- HangmanGame/src/hangmanGame/WriteWordToFile.java
+++ HangmanGame/src/hangmanGame/WriteWordToFile.java
@@ -4,18 +4,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
+import java.util.Scanner;
 
 public class WriteWordToFile {
-	// write a word to dictionary(csv file)
+	// write a word and its definition to dictionary(.csv file)
 	public boolean writeAWordToDictionary(String word) {
 
 		FileWriter pw;
 		try { 
-
 			pw = new FileWriter("dictionary.csv",true);
 			CSVPrinter csvPrinter = new CSVPrinter( pw, CSVFormat.DEFAULT );
-			
-			/*
+
 			char[] lettersOfWord = word.toCharArray();
 			for(char ch : lettersOfWord) {
 				if( (ch-'a' < 0) || (ch-'a' > 26) ) {
@@ -25,11 +24,15 @@ public class WriteWordToFile {
 					return false;
 				}
 			}
-			*/
-			
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Type the definition of this word:");
+			String definition =  scan.nextLine();
+			csvPrinter.printRecord((String) word,"x",definition);
+
 			csvPrinter.flush();
 			csvPrinter.close();
 			pw.close();
+			scan.close();
 			return true;
 
 		} catch (IOException e) {
